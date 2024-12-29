90809b5c99100a7e649fc95a1504cb7aae67a9e8
Ignacio Salcedo <salcedo.ig82@gmail.com>
2024-12-29 17:29:46 -0300

    Mejora en Excepciones RabbitMq

--- ms-ventas/src/main/java/com/app/eclub/ventas/data/service/SaleServiceImpl.java
+++ ms-ventas/src/main/java/com/app/eclub/ventas/data/service/SaleServiceImpl.java
@@ -1,7 +1,11 @@
 package com.app.eclub.ventas.data.service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -9,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.app.eclub.ventas.configure.RabbitConfig;
 import com.app.eclub.ventas.data.entity.Sale;
 import com.app.eclub.ventas.data.repository.SaleRepository;
+import com.app.eclub.ventas.exception.RabbitMqNotFountException;
 import com.app.eclub.ventas.exception.StockNotFountException;
 import com.app.eclub.ventas.rest.client.StockClient;
 import com.app.eclub.ventas.rest.dto.ProductDto;
@@ -35,27 +40,44 @@ public class SaleServiceImpl implements SaleService{
 
             List<ProductDto> detailProductStock = stockClientRest.findAllById(detailSales);
 
-            detailProductStock.forEach(productDto ->{
-                saleDto.getDetails().stream()
-                .filter(detail -> detail.getCodProduct().equals(productDto.getProductId()))
-                .findFirst()
-                .ifPresent(saleDetail -> {
-                    if(saleDetail.getAmountSale().compareTo(productDto.getQuantity())>0){
-                        throw new StockNotFountException("The quantity is  the product " + saleDetail.getCodProduct() + " is insufficient");
-                    }
-                });
-            });
 
+            // Crear un mapa para acceder rápidamente al stock disponible
+            Map<String, BigDecimal> productStockMap = detailProductStock.stream()
+                .collect(Collectors.toMap(ProductDto::getProductId, ProductDto::getQuantity));
+
+            // Verificar si la cantidad de productos en la venta es mayor a la cantidad en stock
+            validateStockAvailability(saleDto, productStockMap);
 
             Sale saleSave = repository.save(factory.converToSaleEntity(saleDto));
 
             SaleDto saleSaveDto= factory.converToSaleDto(saleSave);
 
-            rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_STOCK_NAME, RabbitConfig.ROUTING_STOCK_KEY, saleSaveDto);
+            sendSaleToStockEchage(saleSaveDto);
 
             return saleSaveDto;
 
         }
+    
+    private void validateStockAvailability(SaleDto saleDto, Map<String, BigDecimal> productStockMap){
+        saleDto.getDetails().forEach(saleDetail -> {
+            BigDecimal availableStock = productStockMap.getOrDefault(saleDetail.getCodProduct(), BigDecimal.ZERO);
+            if(saleDetail.getAmountSale().compareTo(availableStock)>0){
+                throw new StockNotFountException("Insufficient stock for product. " + 
+                saleDetail.getCodProduct() +
+                " Requested: " + 
+                saleDetail.getAmountSale() 
+                + ", Available: " +
+                availableStock);
+            }
+        });
+    }
+    private void sendSaleToStockEchage(SaleDto saleDtoSave){
+        try {
+            rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_STOCK_NAME, RabbitConfig.ROUTING_STOCK_KEY, saleDtoSave);
+        } catch (AmqpException e) {
+            throw new RabbitMqNotFountException("The operation could not be completed. The broker cannot communicate");
+        }
+    }
 
     @Override
     public ProductDto getProductById(String idProduct) {
